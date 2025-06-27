3e687f5ab2b2d6db9a4c596b60502ade4a2c4834
harishkannarao <harishkannarao@users.noreply.github.com>
2025-06-27 10:27:12 +0100

    Grouped by chapters

--- src/main/java/com/harishkannarao/thirukkural/data/JsonStructureTransformer.java
+++ src/main/java/com/harishkannarao/thirukkural/data/JsonStructureTransformer.java
@@ -7,10 +7,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Component
 public class JsonStructureTransformer {
@@ -25,9 +31,7 @@ public class JsonStructureTransformer {
     public JsonStructureTransformer(
             ObjectMapper objectMapper,
             @Value("${input.json}") String inputJsonPath,
-            @Value("${output.json}") String outputJsonPath)
-
-    {
+            @Value("${output.json}") String outputJsonPath) {
         this.objectMapper = objectMapper;
         this.inputJsonPath = inputJsonPath;
         this.outputJsonPath = outputJsonPath;
@@ -37,9 +41,13 @@ public class JsonStructureTransformer {
         LOG.info("Transforming raw json to structured json");
         try {
             String inputJson = Files.readString(Paths.get(inputJsonPath));
-            JsonNode jsonNode = objectMapper.readTree(inputJson);
-            String jsonString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
-            Files.writeString(Paths.get(outputJsonPath), jsonString);
+            JsonNode rootNode = objectMapper.readTree(inputJson);
+            if (rootNode.isArray()) {
+                Map<String, List<JsonNode>> groupedByChapters = StreamSupport.stream(rootNode.spliterator(), false)
+                        .collect(Collectors.groupingBy(element -> element.get("adikaram_transliteration").asText()));
+                String jsonString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(groupedByChapters);
+                Files.writeString(Paths.get(outputJsonPath), jsonString);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
