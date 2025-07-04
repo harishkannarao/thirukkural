7e113a735c01920aeb2e860c75f5afa5d34e97d1
harishkannarao <harishkannarao@users.noreply.github.com>
2025-07-04 15:32:54 +0100

    Reading Books

--- src/main/java/com/harishkannarao/thirukkural/epub/EpubCreator.java
+++ src/main/java/com/harishkannarao/thirukkural/epub/EpubCreator.java
@@ -1,12 +1,19 @@
 package com.harishkannarao.thirukkural.epub;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.harishkannarao.thirukkural.model.Book;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @ConditionalOnProperty(name = "task", havingValue = "create_book")
@@ -32,5 +39,21 @@ public class EpubCreator {
 
     public void createBook() {
         log.info("Creating EPUB book");
+        log.info("Base {}", baseJson);
+        log.info("Other languages {}", otherLanguages);
+        Book base = readJsonBook(baseJson);
+        List<Book> otherLanguages = Arrays.stream(this.otherLanguages.split(","))
+                .map(this::readJsonBook)
+                .toList();
+
+    }
+
+    private Book readJsonBook(String file) {
+        try {
+            String json = Files.readString(Paths.get(file));
+            return objectMapper.readValue(json, Book.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
